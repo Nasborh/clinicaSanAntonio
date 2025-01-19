acccdad2f4de3323e7e349891d9c42195a8307e7
Nasborh <lindimarescobar@gmail.com>
2025-01-19 12:17:43 -0400

    act citas

--- my-project/src/pages/Citas.tsx
+++ my-project/src/pages/Citas.tsx
@@ -1,13 +1,13 @@
 export function Citas({ isOpen }) {
     return (
-        <section className={`flex flex-col ${isOpen ? 'mt-16' : 'mt-2'}`} >
+        <section className={`flex flex-col ${isOpen ? 'mt-16' : 'mt-0'}`} >
         <div className="min-h-screen bg-gray-100">
             {/* Encabezado  */}
-            <div className="relative bg-blue-600 text-white">
+            <div className="relative bg-sky-500 text-white">
                 <div className="absolute inset-0 bg-cover bg-center"
                     Style={"background-image: url('../assets/imagen_clinica.jpg')"}></div>
-                <div className="relative bg-opacity-75 bg-blue-600 p-6 text-center">
-                    <h1 className="text-4xl font-bold">Clínica Salud Total</h1>
+                <div className="relative bg-opacity-75 bg-sky-500 p-6 text-center">
+                    <h1 className="text-4xl font-bold">Clínica San Antonio</h1>
                     <p className="text-lg mt-2">Solicita tu cita en línea de manera rápida y sencilla</p>
                 </div>
             </div>
@@ -57,14 +57,14 @@ export function Citas({ isOpen }) {
 
                                 <div className="relative max-w-sm">
                                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                        <svg className="w-4 h-4 text-gray-500" aria-hidden="true"
                                             xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                             <path
                                                 d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                                         </svg>
                                     </div>
                                     <input id="datepicker-autohide" datepicker datepicker-autohide type="text"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
                                         placeholder="Select date"></input>
                                 </div>
                             </div>
@@ -99,7 +99,7 @@ export function Citas({ isOpen }) {
                         {/* Botón  */}
                         <div>
                             <button type="submit"
-                                className="w-full bg-blue-600 text-white font-bold py-2 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                                className="w-full bg-sky-500 text-white font-bold py-2 rounded-lg shadow-md hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                                 Enviar Solicitud
                             </button>
                         </div>
