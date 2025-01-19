2ae29f4765b49be300bd9c172e1940d756b1182a
Nasborh <lindimarescobar@gmail.com>
2025-01-18 20:13:26 -0400

    home

--- my-project/src/pages/Footer.tsx
+++ my-project/src/pages/Footer.tsx
@@ -23,7 +23,7 @@ export function Footer(){
                 <SeccionesFooterText posicion={info[3]}/>
                 </div>
 
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr className="my-6 border-gray-200 sm:mx-auto" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500  sm:text-center">© 2025 Todos los derechos reservados.</span>
                     <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
--- my-project/src/pages/Home.tsx
+++ my-project/src/pages/Home.tsx
@@ -37,6 +37,8 @@ export function Home() {
 
             </div>
 
+            <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+
 
 
 
