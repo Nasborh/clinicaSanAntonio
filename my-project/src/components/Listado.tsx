4bacf223e1d53d9a3760ea91725224c12263f578
Rodolfo Ramirez <ramirezrodolfo333@gmail.com>
2025-01-20 01:45:18 -0400

    Server_and_Express

--- my-project/src/components/Listado.tsx
+++ my-project/src/components/Listado.tsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
-import  {info } from "../Models/Servicios";
+import { info } from "../Models/Servicios";
 
-export function Listado({ruta}: {ruta: string}) {
+export function Listado({ ruta }: { ruta: string }) {
     return (
 
         info.map(servicio =>
             <li className="mb-4">
-                 <Link to={ruta} className="hover:underline">{servicio.Nombre}</Link>
+                <Link to={ruta} className="hover:underline">{servicio.Nombre}</Link>
             </li>
 
-         )
-    )       
+        )
+    )
 }
--- my-project/src/pages/Citas.tsx
+++ my-project/src/pages/Citas.tsx
@@ -1,4 +1,8 @@
-export function Citas({ isOpen }) {
+interface CitasProps {
+    isOpen: boolean;
+}
+
+export function Citas({ isOpen }: CitasProps) {
     return (
         <section className={`flex flex-col ${isOpen ? 'mt-16' : 'mt-0'}`} >
             <div className="min-h-screen bg-gray-100">
@@ -15,17 +19,17 @@ export function Citas({ isOpen }) {
                 {/* htmlFormulario */}
                 <div className="container mx-auto py-12 px-4 lg:px-8">
                     <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-8">
-                        <h2 className="text-2xl font-semibold text-gray-800 mb-6">Formulario de Solicitud de Cita</h2>
+                        <h2 className="text-2xl font-semibold text-blue-900 mb-6">Formulario de Solicitud de Cita</h2>
                         <form className="space-y-6">
                             {/* Especialidad  */}
                             <div className="flex items-center space-x-4">
-                                <div className="flex-shrink-0">
-                                    <i className="fas fa-stethoscope text-blue-500 text-2xl"></i>
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-stethoscope text-sky-500 text-2xl"></i>
                                 </div>
                                 <div className="w-full">
-                                    <label htmlFor="specialty" className="block text-sm font-medium text-gray-700">Especialidad</label>
-                                    <select id="specialty" name="specialty"
-                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+                                    <label htmlFor="specialty" className="block text-sm font-semibold text-sky-500">Especialidad</label>
+                                    <select id="specialty" name="specialty" required
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500">
                                         <option value="" disabled selected>Seleccionar especialidad</option>
                                         <option>Cardiología</option>
                                         <option>Dermatología</option>
@@ -36,48 +40,37 @@ export function Citas({ isOpen }) {
                             </div>
                             {/* Médico */}
                             <div className="flex items-center space-x-4">
-                                <div className="flex-shrink-0">
-                                    <i className="fas fa-user-md text-blue-500 text-2xl"></i>
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-user-md  text-sky-500 text-2xl"></i>
                                 </div>
                                 <div className="w-full">
-                                    <label htmlFor="doctor" className="block text-sm font-medium text-gray-700">Médico</label>
-                                    <select id="doctor" name="doctor"
-                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+                                    <label htmlFor="doctor" className="block text-sm font-semibold text-sky-500">Médico</label>
+                                    <select id="doctor" name="doctor" required
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2.5">
                                         <option value="" disabled selected>Seleccionar médico</option>
                                     </select>
                                 </div>
                             </div>
                             {/* Calendario  */}
                             <div className="flex items-center space-x-4">
-                                <div className="flex-shrink-0">
-                                    <i className="fas fa-calendar-alt text-blue-500 text-2xl"></i>
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-calendar-alt text-sky-500 text-2xl"></i>
                                 </div>
                                 <div className="w-full">
-                                    <label htmlFor="date" className="block text-sm font-medium text-gray-700">Fecha de la cita</label>
-
-                                    <div className="relative max-w-sm">
-                                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                                <path
-                                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                                            </svg>
-                                        </div>
-                                        <input id="datepicker-autohide" datepicker datepicker-autohide type="text"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
-                                            placeholder="Select date"></input>
-                                    </div>
+                                    <label htmlFor="date" className="block text-sm font-semibold text-sky-500">Fecha de la cita</label>
+                                    <input type="date" required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+                                    </input>
                                 </div>
                             </div>
                             {/* Horario  */}
                             <div className="flex items-center space-x-4">
-                                <div className="flex-shrink-0">
-                                    <i className="fas fa-clock text-blue-500 text-2xl"></i>
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-clock text-sky-500 text-2xl"></i>
                                 </div>
                                 <div className="w-full">
-                                    <label htmlFor="schedule" className="block text-sm font-medium text-gray-700">Horario</label>
-                                    <select id="schedule" name="schedule"
-                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+                                    <label htmlFor="schedule" className="block text-sm font-semibold text-sky-500">Horario</label>
+                                    <select id="schedule" name="schedule" required
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500">
                                         <option value="" disabled selected>Seleccionar horario</option>
                                         <option>08:00 AM - 10:00 AM</option>
                                         <option>10:00 AM - 12:00 PM</option>
@@ -87,13 +80,83 @@ export function Citas({ isOpen }) {
                             </div>
                             {/* Nombre */}
                             <div className="flex items-center space-x-4">
-                                <div className="flex-shrink-0">
-                                    <i className="fas fa-user text-blue-500 text-2xl"></i>
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-user text-sky-500 text-2xl"></i>
                                 </div>
                                 <div className="w-full">
-                                    <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nombre</label>
-                                    <input type="text" id="name" name="name"
-                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"></input>
+                                    <label htmlFor="name" className="block text-sm font-semibold text-sky-500">Nombre</label>
+                                    <input type="text" id="name" name="name" required
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500"></input>
+                                </div>
+                            </div>
+                            {/* Apellido */}
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-user text-sky-500 text-2xl"></i>
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="last_name" className="block text-sm font-semibold text-sky-500">Apellido</label>
+                                    <input type="text" id="last_name" name="last_name" required
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500"></input>
+                                </div>
+                            </div>
+                            {/* Sexo  */}
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fa fa-venus-mars text-sky-500 text-2xl"></i>
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="gender" className="block text-sm font-semibold text-sky-500">Sexo</label>
+                                    <select id="gender" name="gender" required
+                                        className="mt-1 block w-full rounded-lg p-2.5 border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+                                        <option value="" disabled selected>Sexo*</option>
+                                        <option value={"M"}>Masculino</option>
+                                        <option value={"F"}>Femenino </option>
+                                    </select>
+                                </div>
+                            </div>
+                            {/* Fecha de nacimiento */}
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-calendar-alt text-sky-500 text-2xl"></i>
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="born" className="block text-sm font-semibold text-sky-500">Fecha de Nacimiento</label>
+                                    <input type="date" required name="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+                                    </input>
+                                </div>
+                            </div>
+                            {/* Cédula / Pasaporte */}
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-id-card text-sky-500 text-2xl"></i>
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="identity_numb" className="block text-sm font-semibold text-sky-500">Cédula / Pasaporte</label>
+                                    <input type="number" required id="identity_numb" name="identity_numb" min={1} max={999999999}
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500"></input>
+                                </div>
+                            </div>
+                            {/* Teléfono */}
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-phone text-sky-500 text-2xl"></i>
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="phone" className="block text-sm font-semibold text-sky-500">Teléfono</label>
+                                    <input type="tel" required id="phone" name="phone"
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500"></input>
+                                </div>
+                            </div>
+                            {/* Correo */}
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0 self-center">
+                                    <i className="fas fa-envelope text-sky-500 text-2xl"></i>
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="email" className="block text-sm font-semibold text-sky-500">Correo</label>
+                                    <input type="email" required id="email" name="email"
+                                        className="mt-1 block w-full rounded-lg border-gray-300 shadow-sm p-2.5 focus:ring-blue-500 focus:border-blue-500" placeholder="Dirección de Correo Electrónico"></input>
                                 </div>
                             </div>
                             {/* Botón  */}
